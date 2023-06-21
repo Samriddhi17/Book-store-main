4b0165a8db1364eec9d70c48999ac00430209936
Samriddhi17 <samruddhiambatkar1234@gmail.com>
2023-06-21 13:31:03 +0530

    search button removed

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const searchInput = document.querySelector("[data-search]")
+const submitBtn = document.querySelector("submit")
 
 searchInput.addEventListener("input", e => {
+    console.log(e);
     const value = e.target.value.toLowerCase()
+    console.log(value)
     users.forEach(user => {
-      const isVisible =
-        user.name.toLowerCase().includes(value) ||
-        user.email.toLowerCase().includes(value)
+      const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
       user.element.classList.toggle("hide", !isVisible)
     })
   })
-  
+
+
+submitBtn.addEventListener("submit", e => {
+  console.log(e);
+})
+
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(res => res.json())
   .then(data => {
